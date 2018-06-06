f1d9f5ec90b5eeda3335ce1feaf95582da3e9265
lilycampbell <lilycatcampbell@gmail.com>
2018-06-06 20:14:45 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,8 +1,21 @@
-def mothers_day 
-  puts "Happy Mothers Day, Mom!"
+def mothers_day (your_name = "Mom")
+  puts "Happy Mothers Day, #{your_name}!"
 end
 
 mothers_day
+
+
+
+
+
+
+
+
+
+def say_hello (your_name = "Ruby Programmer")
+  puts "Hello #{your_name}!"
+end
+
 
 
 
